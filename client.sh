5c54afb0303755967aff9c2f14c42a83eac7b8a4
Radhika <radhika@cs.iitr.ac.in>
2019-10-28 20:55:09 +0530

    Add bash script to setup ssh on client

---
+++ client.sh
@@ -0,0 +1,29 @@
+#Add the client computer's IP Addresses to /etc/hosts
+cat /etc/hosts;
+
+#This will create a new user named mpiuser 
+sudo adduser mpiuser;
+#This will let you set password for mpiuser
+sudo passwd mpiuser;
+
+
+
+#Setting up SSH
+
+#install ssh
+sudo dnf install -y openssh-server;
+#start ssh service on device
+sudo systemctl start sshd.service;
+sudo systemctl enable sshd.service;
+#login to the created user 'mpiuser'
+su - mpiuser
+
+#Generate SSH keys and copy them to the machines' list of authorized_keys
+ssh-keygen -t rsa
+
+ssh mpiuser@master mkdir -p .ssh
+cat .ssh/id_rsa.pub | ssh mpiuser@master 'cat >> .ssh/authorized_keys'
+ssh mpiuser@master 'chmod 700 .ssh'
+chmod 640 .ssh/authorized_keys
+exit
+
